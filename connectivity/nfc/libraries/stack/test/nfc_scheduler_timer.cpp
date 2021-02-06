111df813a77840e6dc5681a1e146658bd8dbdaea
Donatien Garnier <donatien.gar@gmail.com>
2021-02-06 18:09:31 +0000

    Add GoogleTest skeleton

---
+++ connectivity/nfc/libraries/stack/test/nfc_scheduler_timer.cpp
@@ -0,0 +1,28 @@
+#include "nfc_scheduler.h"
+#include "nfc_scheduler_timer.h"
+
+static uint32_t timer_value = 0;
+
+void nfc_scheduler_timer_fake_set_value(uint32_t value) {
+    timer_value = value;
+}
+
+void nfc_scheduler_timer_init(nfc_scheduler_timer_t *timer) {
+
+}
+
+void nfc_scheduler_timer_start(nfc_scheduler_timer_t *timer) {
+
+}
+
+uint32_t nfc_scheduler_timer_get(nfc_scheduler_timer_t *timer) {
+    return timer_value;
+}
+
+void nfc_scheduler_timer_stop(nfc_scheduler_timer_t *timer) {
+
+}
+
+void nfc_scheduler_timer_reset(nfc_scheduler_timer_t *timer) {
+
+}
---
+++ connectivity/nfc/libraries/stack/test/nfc_scheduler_timer.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <cstdint>
+
+#include "nfc_scheduler.h"
+void nfc_scheduler_timer_fake_set_value(uint32_t value);
